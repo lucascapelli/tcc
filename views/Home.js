3cc4c9b3b3e47c671242e71502845c3ac0ecc5e2
luscapelli <alcantaraminacapellilucas@gmail.com>
2024-10-25 20:28:22 -0300

    alteração das dependencias json

--- views/Home.js
+++ views/Home.js
@@ -9,16 +9,6 @@ const Home = () => {
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
-});
+
 
 export default Home;
