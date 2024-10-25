abc8c1d1a078ed37cef4aea424135c2ab3ec26f1
luscapelli <alcantaraminacapellilucas@gmail.com>
2024-10-25 20:49:35 -0300

    modelo home atualizado styles e porta 3000

--- views/Home.js
+++ views/Home.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import axios from 'axios';
+import styles from '../views/styles'; // Importa o arquivo de estilos
+
 
 const Home = () => {
+    const [rotas, setRotas] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/rotas')
+            .then(response => setRotas(response.data))
+            .catch(error => console.log(error));
+    }, []);
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Bem-vindo à Home!</Text>
+            {rotas.map(rota => (
+                <Text key={rota.ID_Rota}>{rota.Nome_Rota}</Text>
+            ))}
         </View>
     );
 };
-
-
 
 export default Home;
