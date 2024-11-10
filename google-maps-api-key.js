c5fc0f52f486f9b1da6e4170bd65d46e8509e3e0
luscapelli <alcantaraminacapellilucas@gmail.com>
2024-11-10 14:56:10 -0300

    mudança do server.js, exclusão do arquivo .js com a chave da api e alocação dela para os .env

--- google-maps-api-key.js
+++
@@ -1,3 +0,0 @@
-// google-maps-api-key.js
-const GOOGLE_MAPS_API_KEY = 'AIzaSyCnkkuH9eq0ABecUR5SDuYXpffVtEXFgRM'; // Sua chave da API
-export default GOOGLE_MAPS_API_KEY;
--- views/Home.js
+++ views/Home.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import axios from 'axios';
-import MapView, { Marker } from '@teovilla/react-native-web-maps'; 
+import MapView, { Marker } from '@teovilla/react-native-web-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import styles from '../views/styles';
-import GOOGLE_MAPS_API_KEY from '../google-maps-api-key';
 
 const Home = () => {
     const [rotas, setRotas] = useState([]);
@@ -15,7 +14,8 @@ const Home = () => {
     useEffect(() => {
         const fetchRotas = async () => {
             try {
-                const response = await axios.get('http://localhost:3000/api/rotas'); // Cuidado com CORS
+                // Faz uma requisição para a rota '/api/rotas' no backend
+                const response = await axios.get('http://192.168.0.7:3000/api/rotas'); 
                 setRotas(response.data);
                 console.log('Rotas recebidas:', response.data);
             } catch (error) {
@@ -42,7 +42,6 @@ const Home = () => {
             ) : (
                 <Text>Nenhuma rota encontrada.</Text>
             )}
-            {/* Renderiza o mapa */}
             <MapView
                 provider="google"
                 style={{ flex: 1, height: 400 }}
@@ -55,19 +54,22 @@ const Home = () => {
             >
                 <Marker coordinate={origin} title="Origem" />
                 <Marker coordinate={destination} title="Destino" />
-                {/* Adiciona a rota ao mapa */}
                 <MapViewDirections
                     origin={origin}
                     destination={destination}
-                    apikey={GOOGLE_MAPS_API_KEY}
+                    apikey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}  // Certifique-se de passar a chave corretamente aqui
                     strokeWidth={3}
                     strokeColor="hotpink"
                     onReady={(result) => {
-                        console.log(result); // Você pode usar esse resultado se necessário
+                        console.log(result);
                     }}
                     onError={(errorMessage) => {
                         console.log('Error', errorMessage);
                     }}
+                    // Endpoint modificado para utilizar o proxy local
+                    mode="DRIVING"
+                    precision="high"
+                    optimizeWaypoints={true}
                 />
             </MapView>
         </View>
